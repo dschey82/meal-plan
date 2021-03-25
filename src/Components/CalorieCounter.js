b43f36ac1adeff5d61f8b6166c5459876204e3dc
Dan Schey <dan.schey@gmail.com>
2021-03-25 14:49:40 -0700

    update

--- src/Components/CalorieCounter.js
+++ src/Components/CalorieCounter.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import MealSubmit from '../Components/MealSubmit.js'
 
-const CalorieCounter = props => (
-    <div>
-        <header>Calorie Counter!</header>
-    </div>
-);
+const CalorieCounter = props => {
+    return (
+        <div style={{display: 'flex', justifyContent:'center', aligItems:'center'}}>
+            <header>Calorie Counter!</header>
+            <MealSubmit />            
+        </div>
+    );
+};
 
 export default CalorieCounter;
